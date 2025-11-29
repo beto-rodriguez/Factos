c1ca938ef776c601b8b580765525f781283309fc
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-11-28 18:21:53 -0600

    add blazor sample

---
+++ samples/basic/BlazorTests/UnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Factos;
+using Factos.Blazor;
+
+namespace BlazorTests;
+
+public class SomeTests
+{
+    [TestMethod]
+    public async Task SuccessfulTest()
+    {
+        var app = (BlazorAppController)AppController.Current;
+
+        var component = await app.NavigateToView<TestedComponent>();
+
+        // experimental idea to interact with the UI
+        // but for now the priority is to test other blazor components
+        // not the DOM.
+        // with this concept we could extend this library to build DOM tests.
+        var buttonText = await component.GetButtonText();
+        Assert.True("click me" == buttonText);
+    }
+
+    [TestMethod]
+    [ExpectedToFail]
+    public void FailedTest()
+    {
+        // when marked as ExpectedToFail, this test will be reported as passed only if it fails
+        Assert.Equal(1 + 1, 3);
+    }
+}
