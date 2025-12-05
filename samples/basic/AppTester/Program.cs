6985148b4a8b6468dec8903f7f0abfe6c34d3d34
Alberto Rodriguez <rodriguezo.alberto@gmail.com>
2025-12-05 09:51:53 -0600

    .

--- samples/basic/AppTester/Program.cs
+++ samples/basic/AppTester/Program.cs
@@ -177,6 +177,7 @@ var settings = new FactosSettings
                 ["maccatalyst", "maui", "maui-ios"]),
             commands: app => [
                 $"{Constants.TASK_COMMAND} cd-at-project",
+                $"dotnet build -f net10.0-ios -c Debug",
                 $"dotnet run -f net10.0-ios -c Debug &", // maybe trimming is too agressive?
                 $"{Constants.TASK_COMMAND} cd-pop"
             ]
