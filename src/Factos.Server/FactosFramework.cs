4d33265d6d7c173b60a631b2ba0a72228d777929
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2025-12-17 23:40:03 -0600

    .

--- src/Factos.Server/FactosFramework.cs
+++ src/Factos.Server/FactosFramework.cs
@@ -91,46 +91,12 @@ internal sealed class FactosFramework
                     appName = $"{testedApp.Uid} ({i++})";
                 var cacheFile = $"{appName}_cache.json";
 
-                // lets try to cache results on discovery to prevent
-                // re-running the app multiple times during test exploration
-                //TestSessionResponse? clientResponse;
-
-                //if (context.Request is RunTestExecutionRequest && File.Exists(cacheFile))
-                //{
-                //    using (var stream = File.OpenRead(cacheFile))
-                //    {
-                //        var cachedResponse = await JsonSerializer.DeserializeAsync(
-                //            stream, JsonGenerationContext.Default.ExecutionResponse) 
-                //                ?? throw new Exception("an error occurred while reading tests cache.");
-                //        clientResponse = new TestSessionResponse(null, cachedResponse);
-                //    }
-                    
-                //    File.Delete(cacheFile);
-                    
-                //    await deviceWriter.Dimmed($"Using cached results for {appName}...", cancellationToken);
-                //}
-                //else
-                //{
                 await deviceWriter.Title($"Starting {appName}...", cancellationToken);
                 await appRunner.StartApp(testedApp, appName, cancellationToken);
 
-                    //clientResponse = await GetActiveProtocolsClientResponse(
-                    //   appName, context, cancellationToken);
+                var ct = new CancellationTokenSource(TimeSpan.FromSeconds(settings.ConnectionTimeout));
+                var nodesStream = GetNodes(appName, context, cancellationToken).WithCancellation(ct.Token);
 
-                //    if (context.Request is DiscoverTestExecutionRequest)
-                //    {
-                //        File.WriteAllText(
-                //            $"{appName}_cache.json",
-                //            JsonSerializer.Serialize(clientResponse.Response, JsonGenerationContext.Default.ExecutionResponse));
-                //        await deviceWriter.Dimmed($"Cached discovered tests for {appName}.", cancellationToken);
-                //    }
-                //}
-
-                //var requestedNodes = context.Request is DiscoverTestExecutionRequest
-                //    ? MTPResultsMapper.ReadNodes(appName, clientResponse.Response.Discovered)
-                //    : MTPResultsMapper.ReadNodes(appName, clientResponse.Response.Results);
-
-                var nodesStream = GetNodes(appName, context, cancellationToken);
                 await foreach (var node in nodesStream)
                     await context.MessageBus.PublishAsync(
                         this, new TestNodeUpdateMessage(context.Request.Session.SessionUid, node));
@@ -178,82 +144,4 @@ internal sealed class FactosFramework
             yield return MTPResultsMapper.ReadNode(appName, node);
         }
     }
-
-    //private async Task<TestSessionResponse> GetActiveProtocolsClientResponse(
-    //    string appName, ExecuteRequestContext context, CancellationToken cancellationToken, int retryCount = 0)
-    //{
-    //    var requests = new List<Task<TestSessionResponse>>();
-
-    //    await deviceWriter.Blue(
-    //        "Waiting for test nodes from the test app...", cancellationToken);
-
-    // listen for test nodes from all protocols
-    //foreach (var protocol in ProtocolosLifeTime.ActiveProtocols)
-    //    requests.Add(protocol
-    //        .RequestClient(appName, context)
-    //        .ContinueWith(r => r.IsFaulted || r.Result == null
-    //            ? new TestSessionResponse(protocol, new(), r.Exception)
-    //            : new TestSessionResponse(protocol, r.Result))
-    //        );
-
-    //var timeoutTask = GetTimeOutTask();
-    //requests.Add(GetTimeOutTask());
-
-    //// wait for the first protocol to return test nodes
-    //var response = await Task.WhenAny(requests);
-
-    //if (response == timeoutTask)
-    //{
-    //    await deviceWriter.Red(
-    //        $"Connection Error.\n" +
-    //        $"No test nodes received from any client within the timeout period of " +
-    //        $"{settings.ConnectionTimeout} seconds.", cancellationToken);
-
-    //    throw new TimeoutException("No test nodes received from any client.");
-    //}
-
-    //if (response.Exception is not null)
-    //{
-    //    await deviceWriter.Red(
-    //        $"Error receiving test nodes from the test app: {response.Exception.Message}\n" +
-    //        $"{response.Exception.StackTrace}", cancellationToken);
-
-    //    throw new InvalidOperationException(
-    //        "Error receiving test nodes from the test app.", response.Exception);
-    //}
-
-    //await deviceWriter.Blue(
-    //    $"{response.Result.Response.Results.Count()} nodes received by {response.Result.Protocol}", cancellationToken);
-
-    //if (response.Result.Protocol is null)
-    //{
-    //    // im not completely sure when this happens, but it happens on ios simulators sometimes
-    //    // maybe a race condition... ToDo: investigate further
-    //    // for now we will retry a few times before failing
-    //    if (retryCount < 3)
-    //    {
-    //        retryCount++;
-    //        await deviceWriter.Green(
-    //            $"No active protocol found, retrying to get test nodes... (attempt {retryCount}/3)", cancellationToken);
-    //        return await GetActiveProtocolsClientResponse(appName, context, cancellationToken, retryCount);
-    //    }
-    //}
-
-    //return response.Result;
-    //}
-
-    //private async Task<TestSessionResponse> GetTimeOutTask()
-    //{
-    //    await  Task.Delay(TimeSpan.FromSeconds(settings.ConnectionTimeout));
-    //    return new(null, new());
-    //}
-
-    //private class TestSessionResponse(
-    //    IServerSessionProtocol? protocol, ExecutionResponse response, Exception? exception = null)
-    //{
-    //    [JsonIgnore]
-    //    public IServerSessionProtocol? Protocol { get; } = protocol;
-    //    public ExecutionResponse Response { get; } = response;
-    //    public Exception? Exception { get; } = exception;
-    //}
 }
