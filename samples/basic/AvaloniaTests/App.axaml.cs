ba73cb9db6afa67ec29e7eef29ac3cb5711f45ea
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2026-01-11 10:18:59 -0600

    force on ios

--- samples/basic/AvaloniaTests/App.axaml.cs
+++ samples/basic/AvaloniaTests/App.axaml.cs
@@ -17,7 +17,11 @@ public partial class App : Application
         // Line below is needed to remove Avalonia data validation.
         // Without this line you will get duplicate validations from both Avalonia and CT
         BindingPlugins.DataValidators.RemoveAt(0);
-        
+
+        // useful to ensure the Factos.SGTests static constructor has run
+        // specially it prevents issues with tests detection on iOS.
+        System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(typeof(Factos.SGTests).TypeHandle);
+
         this.UseFactosApp();
 
         base.OnFrameworkInitializationCompleted();
